91425a29b05044810fe9d2eb094f4433680f7145
Ankit Khanal <ankitkhanal231@gmail.com>
2024-06-27 11:43:59 +0545

    linear search

--- linear_search.py
+++ linear_search.py
@@ -1,2 +1,32 @@
 def linear_search(li: list, target):
-    pass
+    """
+    Returns index of the element's position in the list
+    """
+
+    # len(li) is a constant time operation meaning calculating it everytime is constant as it is said by py docs (len is cached so it wouldn't matter how many times we call it)
+    # subscripting (i.e, li[1] or li[33]) is also a constant time operation as said by the docs
+    for i in range(0, len(li)):
+        if li[i] == target:
+            return i
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("Found at index:", index)
+    else:
+        print("Not found in the list")
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = linear_search(numbers, 1)
+
+verify(result)
+
+"""
+Why this program is an algorithm?
+- returns a result in each case
+- executes in a finite time
+- returns expected result
+"""
